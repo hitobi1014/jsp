406da4818b89364aa59ca9287384cea792de592e
hitobi1014 <hitobi1014@gmail.com>
2020-10-21 09:25:16 +0900

    Listener  . 리스너 종류  . SessionListener,  SessionAttributeListener 등록  . userMap.jsp  . ServletContextListener 등록

---
+++ src/main/java/kr/or/ddit/login/Logout.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.ddit.listener.SessionAttributeListener;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+}
