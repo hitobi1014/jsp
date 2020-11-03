eaa3c6959195445f995215ae19c2987a3bdbf9af
hitobi1014 <hitobi1014@gmail.com>
2020-11-03 15:16:35 +0900

    타입변환  . 리터럴, 리터럴 표기법  . 타입변환 - property editor  . 타입변환 - conversion service, formatting conversion service

---
+++ src/main/java/kr/or/ddit/config/type/DateStringConverter.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.config.type;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class DateStringConverter implements Converter<String, Date>{
+
+	@Override
+	public Date convert(String source) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = sdf.parse(source);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+			
+		return date;
+	}
+
+}
